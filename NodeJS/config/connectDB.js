82e1db3353f18d660feeb074ee423b619d862d2f
Vuong The Anh <64anhsden@gmail.com>
2026-05-23 16:01:27 +0700

    feat: update Sequelize to support dynamic Docker env variables with local fallback

--- NodeJS/config/connectDB.js
+++ NodeJS/config/connectDB.js
@@ -1,10 +1,20 @@
 const { Sequelize } = require('sequelize');
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('fullstack', 'root', '123456', {
-    host: '127.0.0.1',
+
+const dbName = process.env.DB_NAME || 'fullstack';
+const dbUser = process.env.DB_USER || 'root';
+const dbPassword = process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : '123456';
+const dbHost = process.env.DB_HOST || '127.0.0.1';
+const dbPort = process.env.DB_PORT || 3307;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
     dialect: "mysql",
     logging: false,
-    port: 3307
+    port: dbPort,
+    timezone: "+07:00",
+    query: {
+        raw: true
+    }
 });
 
 let connectDB = async () => {
@@ -15,4 +25,4 @@ let connectDB = async () => {
         console.error('Unable to connect to the database:', error);
     }
 }
-module.exports = connectDB
+module.exports = connectDB;
--- NodeJS/models/index.js
+++ NodeJS/models/index.js
@@ -10,11 +10,22 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const dbName = process.env.DB_NAME || config.database;
+const dbUser = process.env.DB_USER || config.username;
+const dbPassword = process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : config.password;
+const dbHost = process.env.DB_HOST || config.host;
+const dbPort = process.env.DB_PORT || config.port;
+
+sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: config.dialect || "mysql",
+  logging: false,
+  port: dbPort,
+  timezone: "+07:00",
+  query: {
+    raw: true
+  }
+});
 
 fs
   .readdirSync(__dirname)
